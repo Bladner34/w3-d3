fe10b33f2c7539e8ee8a2f55e55286cf1380b4fe
BLadner34 <brittnee.hallman@yahoo.com>
2026-02-20 20:56:40 -0600

    added outputs jsonForm event and try/catch

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 'use strict';
 
-const myh2 = document.getElementById("myh2");
-console.log(myh2)
+const output = document.getElementById ("output");
+const output2 = document.getElementById("output2");
+const output3 = document.getElementById("output3");
+
+const jsonForm = document.getElementById("jsonForm");
+
+jsonForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    console.log(jsonForm.jsonInput.value);
+    try {
+        const items = JSON.parse(jsonForm.jsonInput.value);
+        output.textContent = items[0];
+        output.textContent = items [1];
+        output.textContent = items [2];
+    } catch (error) {
+        
+    }
+})
